71275ae8e65fd40a8f13ab9cb650ad360eb4b025
ianwhitehill <smwirbw@gmail.com>
2020-11-10 09:42:26 -0600

    Start of porject UI

---
+++ JobBoard.MVC.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobBoard.MVC.UI.Startup))]
+namespace JobBoard.MVC.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
