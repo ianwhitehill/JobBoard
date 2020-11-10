a6156c60418e0a302a39b86f9588f0d9ad641cdf
ianwhitehill <smwirbw@gmail.com>
2020-11-10 11:41:21 -0600

    installed Identity Samples and updated web config connection string to data base

--- JobBoard.MVC.UI/Startup.cs
+++ JobBoard.MVC.UI/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(JobBoard.MVC.UI.Startup))]
 namespace JobBoard.MVC.UI
 {
     public partial class Startup
