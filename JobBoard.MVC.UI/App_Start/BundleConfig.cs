c21ac6ced1ce42e7d47836a22a235b7856006708
ianwhitehill <smwirbw@gmail.com>
2020-11-10 20:15:05 -0600

    Template convert

--- JobBoard.MVC.UI/App_Start/BundleConfig.cs
+++ JobBoard.MVC.UI/App_Start/BundleConfig.cs
@@ -19,12 +19,27 @@ namespace JobBoard.MVC.UI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/js/jquery.min.js",
+                      "~/Scripts/js/popper.min.js",
+                      "~/Scripts/js/bootstrap.min.js",
+                      "~/Scripts/js/jquery.cookie.js",
+                      "~/Scripts/js/jquery.waypoints.min.js",
+                      "~/Scripts/js/jquery.counterup.min.js",
+                      "~/Scripts/js/owl.carousel.min.js",
+                      "~/Scripts/js/owl.carousel2.thumbs.min.js",
+                      "~/Scripts/js/jquery.parallax-1.1.3.js",
+                      "~/Scripts/js/bootstrap-select.min.js",
+                      "~/Scripts/js/jquery.scrollTo.min.js",
+                      "~/Scripts/js/front.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/font-awesome.min.css",
+                      "~/Content/css/bootstrap-select.min.css",
+                      "~/Content/css/owl.carousel.css",
+                      "~/Content/css/owl.theme.default.css",
+                      "~/Content/css/style.default.css",
+                      "~/Content/css/custom.css"));
         }
     }
 }
